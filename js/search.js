d3bdc8fcbb7c9802adc1fc3dd375bf15e851397c
olchikdank <olhadankovska@gmail.com>
2015-04-06 22:18:43 +0300

    okeyy

--- js/search.js
+++ js/search.js
@@ -1,26 +1,59 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-	var temp = document.querySelector("#searchInput").value.toLowerCase();
-	var good = document.querySelector("#name").value;
+//	var temp = document.querySelector("#searchInput").value.toLowerCase();
 	
-	searchInput.onfocus = function() {
+//	var good = document.querySelector("#name").value;
+	
+/*	searchInput.onfocus = function() {
 		if (this.className == 'searchInput') {
 			prepareInput(this);
 		}
 	}
 	function prepareInput(searchInput) { // превратить элемент в простой пустой input
 		searchInput.value = '';
-	}
-		
+	}*/
+	
 	searchButton.addEventListener("click", function(){
-			//
+		var temp = document.querySelector("#searchInput").value;
 		if (temp == ""){
 			alert ("Error! Enter data for search!");
+			return;
 		}
+		
+		var supported = false;
+        var found = false;
+		var item = document.querySelector(".grid").value;
+        if (item.find) {        // Firefox, Google Chrome, Safari
+            supported = true;
+			found = item.find (temp);
+        }
+		
 		else {
-			
-			
-		}
+            if (document.selection && document.selection.createRange) { // Internet Explorer, Opera before version 10.5
+				var textRange = document.selection.createRange ();
+                if (textRange.findText) {   // Internet Explorer
+					supported = true;
+					if (textRange.text.length > 0) {
+						textRange.collapse (true);
+						textRange.move ("character", 1);
+					}
+
+					found = textRange.findText (temp);
+					if (found) {
+						textRange.select ();
+					}
+                }
+            }
+        }
+		
+		if (supported) {
+            if (!found) {
+                alert ("The following text was not found:\n" + temp);
+            }
+        }
+        else {
+            alert ("Your browser does not support this example!");
+        }		
 		
 	}); 
 	
